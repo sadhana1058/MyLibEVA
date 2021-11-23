c2060bf0deff530ad63342b92d8bebfac69ef0f5
sadhana1058 <47341316+sadhana1058@users.noreply.github.com>
2021-11-23 16:09:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,39 @@
 import torch
-from tqdm import tqdm
-
-import torch.optim as optim
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
 
-train_losses = []
-train_acc = []
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+import matplotlib.pyplot as plt
 
-from tqdm import tqdm
 
-train_losses = []
-test_losses = []
-
-def trainNetwork(net,epoch, optimizer,criterion,trainloader):
-  net.train()
-  pbar = tqdm(trainloader)
-  correct = 0
-  processed = 0
-  for batch_idx, (data, target) in enumerate(pbar):    
-    # get samples
-    data, target = data.to(device), target.to(device)
-    # Init
-    optimizer.zero_grad()
-    # Predict
-    y_pred = net(data)
-    # Calculate loss
-    loss = criterion(y_pred, target)
-    train_losses.append(loss)
+def training_the_model(no_of_epoch, net, criterion, optimizer, device, trainloader, testloader, best_acc, scheduler):
+    train_loss = []
+    train_acc = []
+    test_loss = []
+    test_acc = []
     
-    # Backpropagation
-    loss.backward()
-    optimizer.step()
-    
-    # Update pbar-tqdm    
-    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-    correct += pred.eq(target.view_as(pred)).sum().item()
-    processed += len(data)
-
-    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Train Accuracy={100*correct/processed:0.2f}')
+    for epoch in range(no_of_epoch):
+        train_loss, train_acc = train(epoch+1, net, criterion, optimizer, device, trainloader, train_loss, train_acc, False)
+        best_acc, test_loss, test_acc = test(epoch+1, net, criterion, device, testloader, best_acc, test_loss, test_acc, False)
+        train_loss, train_acc = train(epoch+1, net, criterion, optimizer, device, trainloader, train_loss, train_acc, True)
+        best_acc, test_loss, test_acc = test(epoch+1, net, criterion, device, testloader, best_acc, test_loss, test_acc, True)
         
-#Testing
-def testNetwork(net, epoch,testloader):
-    global best_acc
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
+    print("Best Acc is : ", best_acc)
+    return train_loss, train_acc, test_loss, 
     
-    test_loss /= len(testloader.dataset)
-    test_losses.append(test_loss)
-    
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total)) 
+def plot_graph(train_l, train_a, test_l, test_a):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_l)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_a)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_l)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_a)
+  axs[1, 1].set_title("Test Accuracy")
 
-
-
-def getTrainLoss():
-    train_losses_cpu = []
-    for loss in train_losses:
-        train_losses_cpu.append(loss.cpu().data.numpy())  
-    
-    return train_losses_cpu
-
-def getTestLoss():
-    return test_losses
